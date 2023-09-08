21f1e2bba9d75659dcc248e34cc3867c723722a9
Zemyt <tijme_@hotmail.com>
2023-09-08 16:14:39 +0200

    add set_date, update report functions

--- functions.py
+++ functions.py
@@ -10,7 +10,6 @@ current_day_file = "date.json"
 
 
 def buy_product(product_name, buy_price, expiration_date):
-
     buy_date = get_current_day(current_day_file).strftime("%Y-%m-%d")
     id = str(get_next_purchace_id())
     data = [id, product_name, buy_date, buy_price, expiration_date]
@@ -18,8 +17,8 @@ def buy_product(product_name, buy_price, expiration_date):
 
     try:
         with open("bought.csv", "a") as file:
-            writer = csv.writer(file)          
-            if file.tell() == 0: # Adds fieldnames if file is empty 
+            writer = csv.writer(file)
+            if file.tell() == 0:  # Adds fieldnames if file is empty
                 writer.writerow(field_names)
             writer.writerow(data)
             print(f"OK: Bought {product_name} for €{buy_price:.2f}.")
@@ -28,11 +27,17 @@ def buy_product(product_name, buy_price, expiration_date):
         print(f"An error occurred: {e}")
 
 
-
 def sell_product(product_name, sell_price):
-
     sell_date = get_current_day(current_day_file).strftime("%Y-%m-%d")
-    field_names = ["id", "product_name", "buy_date", "sell_date", "buy_price", "sell_price", "expiration_date"]
+    field_names = [
+        "id",
+        "product_name",
+        "buy_date",
+        "sell_date",
+        "buy_price",
+        "sell_price",
+        "expiration_date",
+    ]
     sold_products = set()
 
     try:
@@ -43,10 +48,18 @@ def sell_product(product_name, sell_price):
             for line in lines:
                 bought_data = line.split(",")
                 if product_name in line and product_name not in sold_products:
-                    data = [bought_data[0], product_name, bought_data[2], sell_date, bought_data[3], sell_price, bought_data[4].strip()]
+                    data = [
+                        bought_data[0],
+                        product_name,
+                        bought_data[2],
+                        sell_date,
+                        bought_data[3],
+                        sell_price,
+                        bought_data[4].strip(),
+                    ]
                     with open("sold.csv", "a") as sold_file:
                         writer = csv.writer(sold_file)
-                        if sold_file.tell() == 0: # Adds fieldnames if file is empty 
+                        if sold_file.tell() == 0:  # Adds fieldnames if file is empty
                             writer.writerow(field_names)
                         writer.writerow(data)
                         print(f"OK: Sold {product_name} for €{sell_price:.2f}")
@@ -61,17 +74,17 @@ def sell_product(product_name, sell_price):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    
 
 # Report functions
 console = Console()
 
-# Inventory 
+# Inventory
 inventory_table = Table(show_header=True, header_style="bold magenta")
 inventory_table.add_column("Product")
 inventory_table.add_column("Count")
 inventory_table.add_column("Buy Price")
 inventory_table.add_column("Expiration Date")
+
 
 def report_inventory(date):
     inventory_list = []
@@ -83,45 +96,81 @@ def report_inventory(date):
 
         for line in bought_lines:
             bought_data = line.strip().split(",")
+            if len(bought_data) < 6:
+                continue
             if bought_data[0] == "id":
-                continue # skip when we iterate over the fieldnames
-            
+                continue  # skip when we iterate over the fieldnames
+
             # If inventory is empty AND the date we are checking is after buy date > append
-            if len(inventory_list) == 0 and reference_date >= datetime.strptime(bought_data[2], "%Y-%m-%d").date(): # if inventory_list is empty appends the item
-                inventory_list.append([bought_data[1], 1, bought_data[3], bought_data[4]])
+            if (
+                len(inventory_list) == 0
+                and reference_date
+                >= datetime.strptime(bought_data[2], "%Y-%m-%d").date()
+            ):  # if inventory_list is empty appends the item
+                inventory_list.append(
+                    [bought_data[1], 1, bought_data[3], bought_data[4]]
+                )
                 continue
 
             # checks if the item matches [product_name, buy_price and expiration_date], if True then adds 1 to the existing count
             found = False
             for item in inventory_list:
-                if bought_data[1] == item[0] and bought_data[3] == item[2] and bought_data[4] == item[3]:
+                if (
+                    bought_data[1] == item[0]
+                    and bought_data[3] == item[2]
+                    and bought_data[4] == item[3]
+                ):
                     item[1] += 1
                     found = True
-                    break  
+                    break
 
             # if not found AND the date we are checking is after buy date > append
-            if not found and reference_date >= datetime.strptime(bought_data[2], "%Y-%m-%d").date():
-                inventory_list.append([bought_data[1], 1, bought_data[3], bought_data[4]])
+            if (
+                not found
+                and reference_date
+                >= datetime.strptime(bought_data[2], "%Y-%m-%d").date()
+            ):
+                inventory_list.append(
+                    [bought_data[1], 1, bought_data[3], bought_data[4]]
+                )
 
         # inventory logic for sold.csv file
         for line in sold_lines:
             sold_data = line.strip().split(",")
+            if len(sold_data) < 6:
+                continue
             if sold_data[0] == "id":
-                continue # skip when we iterate over the fieldnames  
+                continue  # skip when we iterate over the fieldnames
             # If inventory is empty AND the date we are checking is after buy date AND before sell date > append
-            if len(inventory_list) == 0 and reference_date >= datetime.strptime(sold_data[2], "%Y-%m-%d").date() and reference_date < datetime.strptime(sold_data[3], "%Y-%m-%d").date():
+            if (
+                len(inventory_list) == 0
+                and reference_date >= datetime.strptime(sold_data[2], "%Y-%m-%d").date()
+                and reference_date < datetime.strptime(sold_data[3], "%Y-%m-%d").date()
+            ):
                 inventory_list.append([sold_data[1], 1, sold_data[4], sold_data[-1]])
-            
+
             # checks if the items matches AND the reference date is after buy date AND before sell date, if True then adds 1 to the existing count
             found = False
             for item in inventory_list:
-                if sold_data[1] == item[0] and sold_data[4] == item[2] and sold_data[-1] == item[3] and reference_date >= datetime.strptime(sold_data[2], "%Y-%m-%d").date() and reference_date < datetime.strptime(sold_data[3], "%Y-%m-%d").date():
+                if (
+                    sold_data[1] == item[0]
+                    and sold_data[4] == item[2]
+                    and sold_data[-1] == item[3]
+                    and reference_date
+                    >= datetime.strptime(sold_data[2], "%Y-%m-%d").date()
+                    and reference_date
+                    < datetime.strptime(sold_data[3], "%Y-%m-%d").date()
+                ):
                     item[1] += 1
                     found = True
                     break
-            
+
             # if not found AND the date we are checking is after buy date > append
-            if not found and reference_date >= datetime.strptime(sold_data[2], "%Y-%m-%d").date() and reference_date < datetime.strptime(sold_data[3], "%Y-%m-%d").date():
+            if (
+                not found
+                and reference_date >= datetime.strptime(sold_data[2], "%Y-%m-%d").date()
+                and reference_date < datetime.strptime(sold_data[3], "%Y-%m-%d").date()
+            ):
                 inventory_list.append([sold_data[1], 1, sold_data[4], sold_data[-1]])
 
     for item in inventory_list:
@@ -137,13 +186,15 @@ def report_revenue(date):
         lines = file.readlines()
         for line in lines:
             data = line.split(",")
+            if len(data) < 6:
+                continue
             if data[0] == "id":
                 continue
             if date == datetime.strptime(data[3], "%Y-%m-%d").date():
                 revenue += float(data[5])
 
     print(f"Revenue for {date}: €{revenue:.2f}")
-            
+
 
 # Profit
 def report_profit(date):
@@ -153,6 +204,8 @@ def report_profit(date):
         lines = file.readlines()
         for line in lines:
             data = line.split(",")
+            if len(data) < 6:
+                continue
             if data[0] == "id":
                 continue
             if date == datetime.strptime(data[3], "%Y-%m-%d").date():
@@ -165,14 +218,21 @@ def report_profit(date):
 used_ids = set()
 new_ids = set()
 
+
 def load_used_ids():
     try:
         with open("used_ids.csv", "r") as file:
             lines = file.readlines()
             for line in lines:
+                line = line.strip()
+                if not line:
+                    continue
+                if not line.isdigit():
+                    continue
                 used_ids.add(int(line))
     except FileNotFoundError:
-         print("The 'used_ids.csv' file was not found.")
+        print("The 'used_ids.csv' file was not found.")
+
 
 def append_new_ids():
     try:
@@ -182,7 +242,7 @@ def append_new_ids():
                 writer.writerow([new_id])
             new_ids.clear()
     except FileNotFoundError:
-         print("The 'used_ids.csv' file was not found.")
+        print("The 'used_ids.csv' file was not found.")
 
 
 def get_next_purchace_id():
@@ -201,17 +261,19 @@ def get_next_purchace_id():
 
 
 # Function to validate the correct date format in the buy parse
-def validate_expiration_date(date):
+def validate_date(date):
     try:
         datetime.strptime(date, "%Y-%m-%d")
         return date
     except ValueError:
-        raise argparse.ArgumentTypeError("Invalid expiration date. Use the format 'YYYY-MM-DD'.")
-    
+        raise argparse.ArgumentTypeError(
+            "Invalid expiration date. Use the format 'YYYY-MM-DD'."
+        )
+
 
 # Time functions
 def get_current_day(filename):
-    try: 
+    try:
         with open(filename, "r") as file:
             data = json.load(file)
             if not data:
@@ -225,6 +287,15 @@ def get_current_day(filename):
 
 def write_current_time(filename, current_day):
     data = {"current_day": current_day.strftime("%Y-%m-%d")}
+    try:
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=4)
+    except FileNotFoundError:
+        print(f"{filename} file not found")
+
+
+def set_date(filename, date):
+    data = {"current_day": date.strftime("%Y-%m-%d")}
     try:
         with open(filename, "w") as file:
             json.dump(data, file, indent=4)
--- superpy.py
+++ superpy.py
@@ -13,29 +13,72 @@ subparsers = parser.add_subparsers(dest="command")
 
 # Buy parser
 buy_parser = subparsers.add_parser("buy", help="Buy product")
-buy_parser.add_argument("-n", "--product-name", type=str, help="Name of the product you buy")
-buy_parser.add_argument("-p", "--price", type=float, help="Price of the product you buy")
-buy_parser.add_argument("-exp", "--expiration-date", type=validate_expiration_date, help="Expiration date of the product you buy (format: 'YYYY-MM-DD')")
+buy_parser.add_argument(
+    "-n", "--product-name", type=str, help="Name of the product you buy"
+)
+buy_parser.add_argument(
+    "-p", "--price", type=float, help="Price of the product you buy"
+)
+buy_parser.add_argument(
+    "-exp",
+    "--expiration-date",
+    type=validate_date,
+    help="Expiration date of the product you buy (format: 'YYYY-MM-DD')",
+)
 
 # Sell parser
 sell_parser = subparsers.add_parser("sell", help="Sell product")
-sell_parser.add_argument("-n", "--product-name", type=str, help="Name of the product you sell")
-sell_parser.add_argument("-p", "--price", type=float, help="Price of the product you sell")
+sell_parser.add_argument(
+    "-n", "--product-name", type=str, help="Name of the product you sell"
+)
+sell_parser.add_argument(
+    "-p", "--price", type=float, help="Price of the product you sell"
+)
 
 # Report parser
-report_parser = subparsers.add_parser("report", help="Generate report [inventory, revenue, profit]")
-report_parser.add_argument("report_type", type=str, help="Specify type of report [inventory, revenue, profit]")
-report_parser.add_argument("--now", action="store_true", help="Show inventory for today")
-report_parser.add_argument("--today", action="store_true", help="Show inventory for today")
-report_parser.add_argument("--yesterday", action="store_true", help="Show inventory for yesterday")
-report_parser.add_argument("--date", type=str, help="Show inventory for specific date (format: 'YYYY-MM-DD')")
+report_parser = subparsers.add_parser(
+    "report", help="Generate report [inventory, revenue, profit]"
+)
+report_parser.add_argument(
+    "report_type", type=str, help="Specify type of report [inventory, revenue, profit]"
+)
+report_parser.add_argument(
+    "--now", action="store_true", help="Show inventory for today"
+)
+report_parser.add_argument(
+    "--today", action="store_true", help="Show inventory for today"
+)
+report_parser.add_argument(
+    "--yesterday", action="store_true", help="Show inventory for yesterday"
+)
+report_parser.add_argument(
+    "--date", type=str, help="Show inventory for specific date (format: 'YYYY-MM-DD')"
+)
 
 # Advance time parser
-advance_time_parser = subparsers.add_parser("advance-time", help="Travel through time, and adjust the time")
-advance_time_parser.add_argument("-adv", "--advance", type=int, help="Advances time by amount of days [-1,0,1,2,etc]")
+advance_time_parser = subparsers.add_parser(
+    "advance-time", help="Travel through time, and adjust the time"
+)
+advance_time_parser.add_argument(
+    "-adv", "--advance", type=int, help="Advances time by amount of days [-1,0,1,2,etc]"
+)
 
 # Set current time parser
-current_time_parser = subparsers.add_parser("set-current-time", help="Set time to current time")
+current_time_parser = subparsers.add_parser(
+    "set-current-time", help="Set time to current time"
+)
+
+# Set Date parser
+set_date_parser = subparsers.add_parser(
+    "set-date",
+    help="Set the date to requested date",
+)
+set_date_parser.add_argument(
+    "-d",
+    "--date",
+    type=validate_date,
+    help="Set the date (format: 'YYYY-MM-DD')",
+)
 
 # Parse
 args = parser.parse_args()
@@ -45,56 +88,55 @@ if args.command == "buy":
 
 if args.command == "sell":
     sell_product(args.product_name, args.price)
-    
-if args.command == "report":
-        
-        if args.report_type.lower() == "inventory":
-            current_date = get_current_day(current_day_file)
-            if args.now or args.today:
-                report_inventory(current_date)
-            elif args.yesterday:
-                yesterday = current_date - timedelta(days=1)
-                report_inventory(yesterday)
-            elif args.date:
-                try:
-                    specific_date = datetime.strptime(args.date, "%Y-%m-%d").date()
-                    report_inventory(specific_date)
-                except ValueError:
-                    print("Incorrect date format. Use format: 'YYYY-MM-DD'.")
-            else:
-                report_inventory(current_date)
 
-        if args.report_type == "revenue":
-            current_date = get_current_day(current_day_file)
-            if args.now or args.today:
-                report_revenue(current_date)
-            elif args.yesterday:
-                yesterday = current_date - timedelta(days=1)
-                report_revenue(yesterday)
-            elif args.date:
-                try:
-                    spef_date = datetime.strptime(args.date, "%Y-%m-%d").date()
-                    report_revenue(spef_date)
-                except ValueError:
-                    print("Incorrect date format. Use format: 'YYYY-MM-DD'. TEST")
-            else:
-                report_revenue(current_date)
-            
-        if args.report_type == "profit":
-            current_date = get_current_day(current_day_file)
-            if args.now or args.today:
-                report_profit(current_date)
-            elif args.yesterday:
-                yesterday = current_date - timedelta(days=1)
-                report_profit(yesterday)
-            elif args.date:
-                try:
-                    spef_date = datetime.strptime(args.date, "%Y-%m-%d").date()
-                    report_profit(spef_date)
-                except ValueError:
-                    print("Incorrect date format. Use format: 'YYYY-MM-DD'. TEST")
-            else:
-                report_profit(current_date)
+if args.command == "report":
+    if args.report_type.lower() == "inventory":
+        current_date = get_current_day(current_day_file)
+        if args.now or args.today:
+            report_inventory(current_date)
+        elif args.yesterday:
+            yesterday = current_date - timedelta(days=1)
+            report_inventory(yesterday)
+        elif args.date:
+            try:
+                specific_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+                report_inventory(specific_date)
+            except ValueError:
+                print("Incorrect date format. Use format: 'YYYY-MM-DD'.")
+        else:
+            report_inventory(current_date)
+
+    if args.report_type == "revenue":
+        current_date = get_current_day(current_day_file)
+        if args.now or args.today:
+            report_revenue(current_date)
+        elif args.yesterday:
+            yesterday = current_date - timedelta(days=1)
+            report_revenue(yesterday)
+        elif args.date:
+            try:
+                spef_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+                report_revenue(spef_date)
+            except ValueError:
+                print("Incorrect date format. Use format: 'YYYY-MM-DD'. TEST")
+        else:
+            report_revenue(current_date)
+
+    if args.report_type == "profit":
+        current_date = get_current_day(current_day_file)
+        if args.now or args.today:
+            report_profit(current_date)
+        elif args.yesterday:
+            yesterday = current_date - timedelta(days=1)
+            report_profit(yesterday)
+        elif args.date:
+            try:
+                spef_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+                report_profit(spef_date)
+            except ValueError:
+                print("Incorrect date format. Use format: 'YYYY-MM-DD'. TEST")
+        else:
+            report_profit(current_date)
 
 
 if args.command == "advance-time":
@@ -108,3 +150,9 @@ if args.command == "set-current-time":
     current_date = date.today()
     write_current_time(current_day_file, current_date)
     print(f"Setting date to the current date: {current_date}")
+
+if args.command == "set-date":
+    if args.date:
+        set_date_to = datetime.strptime(args.date, "%Y-%m-%d")
+        set_date(current_day_file, set_date_to)
+        print(f"Setting date to: {args.date}.")
